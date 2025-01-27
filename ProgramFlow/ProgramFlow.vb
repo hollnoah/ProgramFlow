03fd4d6acefd1ac088b9928666284e476f1f252a
Noah Holloway <noahholloway23@gmail.com>
2025-01-27 08:50:30 -0700

    changed variable raccoon to myNumber

--- ProgramFlow/ProgramFlow.vb
+++ ProgramFlow/ProgramFlow.vb
@@ -3,47 +3,62 @@ Option Strict On
 
 Module ProgramFlow
     Sub Main()
-        Dim raccoon As Integer
+        Dim myNumber As Integer
+        Dim userInput As String
 
         'assign 5 to my number (raccoon)
-        raccoon = 5
+        myNumber = 5
 
         'test is raccoon greater than 6
         'display the result on the console
-        If raccoon > 6 Then
-            Console.WriteLine(raccoon > 6)
+        If myNumber > 6 Then
+            Console.WriteLine(myNumber > 6)
         End If
 
-        If raccoon > 3 Then
-            Console.WriteLine(raccoon > 3)
+        If myNumber > 3 Then
+            Console.WriteLine(myNumber > 3)
         End If
 
-        If raccoon > 4 Then
-            Console.WriteLine(raccoon > 4)
+        If myNumber > 4 Then
+            Console.WriteLine(myNumber > 4)
         End If
 
         'only evaluates to see if one of the statements is true. if so, it doesn't read anything else
-        If raccoon > 6 Then
-            Console.WriteLine(raccoon > 6)
-        ElseIf raccoon > 3 Then
-            Console.WriteLine(raccoon > 3)
-        ElseIf raccoon > 4 Then
-            Console.WriteLine(raccoon > 4)
+        If myNumber > 6 Then
+            Console.WriteLine(myNumber > 6)
+        ElseIf myNumber > 3 Then
+            Console.WriteLine(myNumber > 3)
+        ElseIf myNumber > 4 Then
+            Console.WriteLine(myNumber > 4)
         End If
 
 
-        raccoon -= 3
+        myNumber -= 3
         'if no conditions are true, console will read else condition
-        If raccoon > 6 Then
-            Console.WriteLine($"{raccoon} is a big number!")
-        ElseIf raccoon > 3 Then
+        If myNumber > 6 Then
+            Console.WriteLine($"{myNumber} is a big number!")
+        ElseIf myNumber > 3 Then
             Console.WriteLine($"Sorry not tall enough...")
-        ElseIf raccoon > 4 Then
+        ElseIf myNumber > 4 Then
             Console.WriteLine($"who cares")
         Else
             Console.WriteLine($"Not sure what happened")
         End If
 
+        Select Case myNumber
+            Case 0 To 3
+
+            Case 4 To 10
+
+            Case 11 To 64
+
+            Case 65 To 99
+
+            Case > 100
+
+            Case Else
+
+        End Select
 
     End Sub
 
