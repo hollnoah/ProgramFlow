1a329d8957e59a43b0ecd3a99748b567c258afc8
Noah Holloway <noahholloway23@gmail.com>
2025-01-27 08:34:42 -0700

    add if, and if, ifelse examples

--- ProgramFlow/ProgramFlow.vb
+++ ProgramFlow/ProgramFlow.vb
@@ -1,6 +1,36 @@
-﻿Module ProgramFlow
+﻿Option Explicit On
+Option Strict On
 
+Module ProgramFlow
     Sub Main()
+        Dim raccoon As Integer
+
+        'assign 5 to my number (raccoon)
+        raccoon = 5
+
+        'test is raccoon greater than 6
+        'display the result on the console
+        If raccoon > 6 Then
+            Console.WriteLine(raccoon > 6)
+        End If
+
+        If raccoon > 3 Then
+            Console.WriteLine(raccoon > 3)
+        End If
+
+        If raccoon > 4 Then
+            Console.WriteLine(raccoon > 4)
+        End If
+
+        'only evaluates to see if one of the statements is true. if so, it doesn't read anything else
+        If raccoon > 6 Then
+            Console.WriteLine(raccoon > 6)
+        ElseIf raccoon > 3 Then
+            Console.WriteLine(raccoon > 3)
+        ElseIf raccoon > 4 Then
+        Console.WriteLine(raccoon > 4)
+        End If
+
 
     End Sub
 
