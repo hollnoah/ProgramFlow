aee7379498009cb2e6c30d1b03ecf17f72be9a12
Noah Holloway <noahholloway23@gmail.com>
2025-01-27 08:54:58 -0700

    commented out top code and added cases of age ranges

--- ProgramFlow/ProgramFlow.vb
+++ ProgramFlow/ProgramFlow.vb
@@ -6,44 +6,48 @@ Module ProgramFlow
         Dim myNumber As Integer
         Dim userInput As String
 
-        'assign 5 to my number (raccoon)
-        myNumber = 5
+        ''assign 5 to my number (raccoon)
+        'myNumber = 5
 
-        'test is raccoon greater than 6
-        'display the result on the console
-        If myNumber > 6 Then
-            Console.WriteLine(myNumber > 6)
-        End If
+        ''test is raccoon greater than 6
+        ''display the result on the console
+        'If myNumber > 6 Then
+        '    Console.WriteLine(myNumber > 6)
+        'End If
 
-        If myNumber > 3 Then
-            Console.WriteLine(myNumber > 3)
-        End If
+        'If myNumber > 3 Then
+        '    Console.WriteLine(myNumber > 3)
+        'End If
 
-        If myNumber > 4 Then
-            Console.WriteLine(myNumber > 4)
-        End If
+        'If myNumber > 4 Then
+        '    Console.WriteLine(myNumber > 4)
+        'End If
 
-        'only evaluates to see if one of the statements is true. if so, it doesn't read anything else
-        If myNumber > 6 Then
-            Console.WriteLine(myNumber > 6)
-        ElseIf myNumber > 3 Then
-            Console.WriteLine(myNumber > 3)
-        ElseIf myNumber > 4 Then
-            Console.WriteLine(myNumber > 4)
-        End If
+        ''only evaluates to see if one of the statements is true. if so, it doesn't read anything else
+        'If myNumber > 6 Then
+        '    Console.WriteLine(myNumber > 6)
+        'ElseIf myNumber > 3 Then
+        '    Console.WriteLine(myNumber > 3)
+        'ElseIf myNumber > 4 Then
+        '    Console.WriteLine(myNumber > 4)
+        'End If
 
 
-        myNumber -= 3
-        'if no conditions are true, console will read else condition
-        If myNumber > 6 Then
-            Console.WriteLine($"{myNumber} is a big number!")
-        ElseIf myNumber > 3 Then
-            Console.WriteLine($"Sorry not tall enough...")
-        ElseIf myNumber > 4 Then
-            Console.WriteLine($"who cares")
-        Else
-            Console.WriteLine($"Not sure what happened")
-        End If
+        'myNumber -= 3
+        ''if no conditions are true, console will read else condition
+        'If myNumber > 6 Then
+        '    Console.WriteLine($"{myNumber} is a big number!")
+        'ElseIf myNumber > 3 Then
+        '    Console.WriteLine($"Sorry not tall enough...")
+        'ElseIf myNumber > 4 Then
+        '    Console.WriteLine($"who cares")
+        'Else
+        '    Console.WriteLine($"Not sure what happened")
+        'End If
+
+        Console.WriteLine("Please enter your age:")
+        userInput Console.ReadLine()
+        myNumber = CInt(userInput)
 
         Select Case myNumber
             Case 0 To 3
